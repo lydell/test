b28a21f013fa1054d1878c24e3d7a86572f14a27
Simon Lydell <simon.lydell@gmail.com>
2026-08-16 16:14:57 +0200

    Remove parametrized and fuzzWithExamples from this branch

--- tests/src/Tests.elm
+++ tests/src/Tests.elm
@@ -170,20 +170,6 @@ testTests =
                     test "" expectPass
                         |> expectTestToFail
             ]
-        , describe "parametrized"
-            [ test "fails with empty list" <|
-                \() ->
-                    parametrized [] "x" expectPass
-                        |> expectTestToFail
-            , test "fails with empty name" <|
-                \() ->
-                    parametrized [ ( "x", () ) ] "" expectPass
-                        |> expectTestToFail
-            , test "fails with empty sub name" <|
-                \() ->
-                    parametrized [ ( "", () ) ] "" expectPass
-                        |> expectTestToFail
-            ]
         , describe "fuzz"
             [ test "fails with empty name" <|
                 \() ->
@@ -204,32 +190,6 @@ testTests =
                         Fuzz.bool
                         ""
                         expectPass
-                        |> expectTestToFail
-            ]
-        , describe "fuzzWithExamples"
-            [ test "fails with fewer than 1 run" <|
-                \() ->
-                    fuzzWithExamples { runs = 0, distribution = noDistribution }
-                        Fuzz.bool
-                        []
-                        "nonpositive"
-                        expectPass
-                        |> expectTestToFail
-            , test "fails with empty name" <|
-                \() ->
-                    fuzzWithExamples { runs = 1, distribution = noDistribution }
-                        Fuzz.bool
-                        []
-                        ""
-                        expectPass
-                        |> expectTestToFail
-            , test "fails with empty sub name" <|
-                \() ->
-                    fuzzWithExamples { runs = 1, distribution = noDistribution }
-                        Fuzz.int
-                        [ ( "", 987461349871874 ) ]
-                        "x"
-                        (\n -> n |> Expect.equal 987461349871874)
                         |> expectTestToFail
             ]
         , describe "Test.todo"
